ce787c56a8f5be36aeb9b5fe5218666979d797bd
jromerr <jose.a.romero.199@gmail.com>
2026-05-28 18:51:04 -0700

    Create function for grid of square divs

---
+++ javascript.js
@@ -0,0 +1,17 @@
+const divContainer = document.querySelector(".container");
+
+function createGrid(size = 16){
+    let total = size * size;
+    for(let i = 0; i < total; i++){
+        const squareDiv = document.createElement("div");
+        squareDiv.classList.add("squareDiv");
+        squareDiv.style.boxSizing = 'border-box';
+        squareDiv.style.width = 'calc(100% * (1/' + size + ')';
+        squareDiv.style.height = 'calc(100% * (1/' + size + ')';
+        squareDiv.style.border = '0.5px solid';
+        squareDiv.style.flex = '0 0 auto';
+        divContainer.appendChild(squareDiv);
+    }
+}
+
+createGrid();
